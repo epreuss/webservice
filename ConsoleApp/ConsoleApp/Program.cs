b85e08931c2a620f8d9c494db3ac26a01c8b9d92
epreuss <epreuss@github.com>
2017-12-07 19:56:37 -0300

    Apps iniciais

---
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ConsoleApp.ServiceRef;
+using ConsoleApp.IPServiceRef;
+using ConsoleApp.GodRef;
+using ConsoleApp.com.w3schools.www;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Connecting to webservice...");
+            BibleWebserviceSoapClient client = new BibleWebserviceSoapClient();
+            //MathServiceSoapClient web = new MathServiceSoapClient();
+            //Console.WriteLine(web.Add(1, 2));
+            Console.WriteLine("Connected. Asking service...");
+            Console.WriteLine();
+            Console.WriteLine("Done.");
+            Console.ReadLine();
+        }
+    }
+}
